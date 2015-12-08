04a253cc2f9e07263b3dd621b6fbb8c251496d9d
antoine magny <mr.magny+github@gmail.com>
2015-12-08 13:42:24 +0100

    refactored day 5

--- day5.js
+++ day5.js
@@ -5,43 +5,28 @@ var allstrings = input.split("\n");
 function killTheNaughty(allstrings){
   niceStrings = [];
   for(var i = 0; i < (allstrings.length - 1); i++){
-    var nice =true
     thestring = allstrings[i]
-
-    if (doubleLetter(thestring) !== true){
-      nice = false
-    }
-    if(countVowels(thestring) < 3){
-      nice = false
-    }
-    if(thestring.match(/ab|cd|pq|xy/)){
-      nice = false
-    }
-    // for (var j = 0; j < thestring; j++){
-
-    // }
-    if(nice==true){
-      niceStrings.push(thestring)
-    }
+    if(doubleLetter(thestring) !== true) continue;
+    if(countVowels(thestring) < 3)       continue;
+    if(thestring.match(/ab|cd|pq|xy/))   continue;
+    niceStrings.push(thestring);
   }
   return niceStrings
 };
 
 function doubleLetter(thestring){
-  var result = false;
   for(var j=0; j < (thestring.length - 1); j++){
     if (thestring.substr(j,1) == thestring.substr(j+1,1)){
-      j = thestring.length;
-      result = true;
+      var result = true;
+      break
     }
   }
-  return result;
+  return result || false;
 };
 
-
 function countVowels(string){
-  var m = string.match(/[aeiou]/gi);
-  return m === null ? 0 : m.length;
+  v = string.match(/[aeiou]/gi);
+  return v === null ? 0 : v.length;
 };
 
 console.log(killTheNaughty(allstrings).length)
